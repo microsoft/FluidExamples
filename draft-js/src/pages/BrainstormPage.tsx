6395471fd0dc177a8aac4bf00898c07a4ad2ca7a
Skyler <skjokiel@microsoft.com>
2020-09-04 15:34:03 -0700

    some changes

---
+++ draft-js/src/pages/BrainstormPage.tsx
@@ -0,0 +1,30 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import React from "react";
+
+import { useParams, useHistory } from "react-router-dom";
+
+import { FluidContext } from "../utils/FluidContext";
+import { NoteroView } from "../partials/NoteroView";
+import { useBrainstormData } from "../utils";
+
+export const BrainstormPage = (props: { new?: boolean }) => {
+  const { id } = useParams<{id}>();
+  const history = useHistory();
+
+  if (props.new) {
+    history.replace(`/${id}`);
+  }
+
+  const context = useBrainstormData(id, props.new);
+
+  return context ? (
+    <FluidContext.Provider value={context}>
+      <NoteroView />
+    </FluidContext.Provider>
+  ) : (
+    <></>
+  );
+};
---
+++ draft-js/src/pages/index.ts
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from "../partials/NoteroView";
+export * from "./Home";
+export * from "./BrainstormPage";
