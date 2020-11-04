ba440efbe686e6c3a8d89aec1e5f0feeab0eb7b5
Jacky Yang <liveid.asw@live.com>
2020-11-04 18:09:30 +0800

    adjust alignment

--- brainstorm/src/partials/List.tsx
+++ brainstorm/src/partials/List.tsx
@@ -30,11 +30,11 @@ export const List: FC<ItemProps> = (props) => {
       <li className="option-list">
       {props.items.map((item) => (
         <label className="item">
-          <input type="radio" name="option" onClick={clickItem} disabled={props.user.userType == UserType.designer}></input>
+          <input className="radio-button" type="radio" name="option" onClick={clickItem} disabled={props.user.userType == UserType.designer}></input>
           <span className="note-text">{item.text}</span>
         </label>
       ))}
-      {props.user.userType == UserType.designer && <Button onClick={createItem}> Add option </Button>}
+      {props.user.userType == UserType.designer && <Button onClick={createItem}> +  Add option </Button>}
       {props.user.userType == UserType.responder && <Button onClick={submit}> Submit </Button>}
       </li>
     </div>
