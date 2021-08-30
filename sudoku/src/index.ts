a18026028fb97d671cf74014595904bebe16a85a
Micah Godbolt <mgodbolt@microsoft.com>
2021-08-30 15:18:07 -0700

    remove sudoku and draft js examples

--- sudoku/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
-export { ISudokuViewProps, SudokuView } from "./react/sudokuView";
-export { SudokuContainer } from "./container";
-export { FluidSudoku } from "./fluidSudoku";
