97413b234657cd2001c3de4e67b26922eb580059
Micah Godbolt <mgodbolt@microsoft.com>
2021-08-30 16:34:06 -0700

    init stuff

--- cra-demo/src/App.js
+++ cra-demo/src/App.js
@@ -4,18 +4,8 @@
  */
 
 import React from "react";
-import { FrsClient, InsecureTokenProvider } from "@fluid-experimental/frs-client";
+import { AzureClient, InsecureTokenProvider } from "@fluid-experimental/frs-client";
 import { SharedMap } from "@fluid-experimental/fluid-framework";
-
-const getContainerId = () => {
-    let isNew = false;
-    if (window.location.hash.length === 0) {
-        isNew = true;
-        window.location.hash = Date.now().toString();
-    }
-    const containerId = window.location.hash.substring(1);
-    return { containerId, isNew };
-};
 
 const localConfig = {
     tenantId: "local",
@@ -24,52 +14,59 @@ const localConfig = {
     storage: "http://localhost:7070",
 };
 
-const getFluidData = async () => {
+const client = new AzureClient(localConfig);
 
-    const { containerId, isNew } = getContainerId();
+const containerSchema = {
+    initialObjects: { myMap: SharedMap }
+};
 
-    const containerSchema = {
-        name: 'cra-demo',
-        initialObjects: { mySharedMap: SharedMap }
-    };
-
-    const client = new FrsClient(localConfig);
-    const { fluidContainer } = isNew
-        ? await client.createContainer({ id: containerId }, containerSchema)
-        : await client.getContainer({ id: containerId }, containerSchema);
-    // returned initialObjects are live Fluid data structures
-    return fluidContainer.initialObjects;
+const useFluidData = () => {
+    const [fluidData, setFluidData] = React.useState();
+    const createContainer = async () => {
+        const { container } = await client.createContainer(containerSchema);
+        const id = container.attach();
+        setFluidData(container.initialObjects);
+        return id;
+    }
+    const loadContainer = async (id) => {
+        const { container } = await client.getContainer(id, containerSchema);
+        setFluidData(container.initialObjects);
+    }
+    React.useEffect(async () => {
+        if (!location.hash) {
+            const id = await createContainer();
+            location.hash = id;
+        } else {
+            const id = location.hash.substring(1);
+            await loadContainer(id)
+        }
+    }, []);
+    return fluidData
 }
 
 function App() {
 
-    const [fluidData, setFluidData] = React.useState();
+    const { myMap } = useFluidData();
     const [viewData, setViewData] = React.useState();
 
     React.useEffect(() => {
-        // Get/Create container and return live Fluid data
-        getFluidData().then(data => setFluidData(data))
-    }, []);
+        if (!myMap) return;
 
-    React.useEffect(() => {
-        if (!fluidData) return;
-
-        const { mySharedMap } = fluidData;
         // sync Fluid data into view state
-        const syncView = () => setViewData({ time: mySharedMap.get("time") });
+        const syncView = () => setViewData({ time: myMap.get("time") });
         // ensure sync runs at least once
         syncView();
         // update state each time our map changes
-        mySharedMap.on("valueChanged", syncView);
-        return () => { mySharedMap.off("valueChanged", syncView) }
+        myMap.on("valueChanged", syncView);
+        return () => { myMap.off("valueChanged", syncView) }
 
-    }, [fluidData])
+    }, [ myMap ])
 
 
     if (!viewData) return <div />;
 
     // business logic could be passed into the view via context
-    const setTime = () => fluidData.mySharedMap.set("time", Date.now().toString());
+    const setTime = () => myMap.set("time", Date.now().toString());
 
     return (
         <div>
