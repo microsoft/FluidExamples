1a002d0c60085d38ac0bc67dd85c9557635dfeca
Micah Godbolt <mgodbolt@microsoft.com>
2021-09-07 10:06:12 -0700

    few tweaks

--- cra-demo/src/App.js
+++ cra-demo/src/App.js
@@ -14,17 +14,17 @@ const containerSchema = {
     initialObjects: { myMap: SharedMap }
 };
 
-const dateKey = "date-key";
+const timeKey = "time-key";
 
 function App() {
 
-    const [fluidData, setFluidData] = React.useState(undefined);
+    const [fluidObjects, setFluidObjects] = React.useState(undefined);
     React.useEffect(() => {
-        const getFluidData = async () => {
+        const getFluidObjects = async () => {
             let container;
             if (location.hash <= 1) {
                 ({ container } = await client.createContainer(containerSchema));
-                container.initialObjects.myMap.set(dateKey, Date.now().toString());
+                container.initialObjects.myMap.set(timeKey, Date.now().toString());
                 const id = await container.attach();
                 location.hash = id;
             } else {
@@ -33,24 +33,25 @@ function App() {
             }
             return container.initialObjects;
         }
-        getFluidData().then(data => setFluidData(data));
+        getFluidObjects().then(objects => setFluidObjects(objects));
     }, []);
 
     const [viewData, setViewData] = React.useState(undefined);
     React.useEffect(() => {
-        if (fluidData !== undefined) {
-            const syncView = () => setViewData({ time: fluidData.myMap.get(dateKey) });
+        if (fluidObjects !== undefined) {
+            const { myMap } = fluidObjects;
+            const syncView = () => setViewData({ time: myMap.get(timeKey) });
             syncView();
-            fluidData.myMap.on("valueChanged", syncView);
-            return () => { fluidData.myMap.off("valueChanged", syncView) }
+            myMap.on("valueChanged", syncView);
+            return () => { myMap.off("valueChanged", syncView) }
         }
-    }, [fluidData])
+    }, [fluidObjects])
 
 
     if (!viewData) return <div />;
 
     // business logic could be passed into the view via context
-    const setTime = () => fluidData.myMap.set(dateKey, Date.now().toString());
+    const setTime = () => fluidObjects.myMap.set(timeKey, Date.now().toString());
 
     return (
         <div>
