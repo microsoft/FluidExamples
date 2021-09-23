04726498e43051d8142a0c22d14e62bdc09ca4b0
Scott Norton <scottnorton@microsoft.com>
2021-09-23 13:54:49 -0400

    Add Angular starter code for example with Epoch time (#299)
    This PR adds a simple example for using Fluid with Angular CLI. This application has a single button to get the UNIX Epoch time and uses a SharedMap to store and synchronize the time between the clients.
    
    * angular starter initial commit
    
    * Updated README for Angular starter example
    
    * Adjustments to angular starter code based on feedback
    
    * remove unncessary files, remove CLI from readme, use await instead of .then()
    
    * Update fluid-angular-starter/src/app/app.component.ts
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    
    * Update fluid-angular-starter/src/app/app.component.ts
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    
    * Update fluid-angular-starter/src/app/app.component.ts
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    
    * Update fluid-angular-starter/src/app/app.component.ts
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    
    * fix lifecycle hook for on component destroy
    
    * store sharedTimestamp directly
    
    * update angular demo package name
    
    * remove react-specific info from angular demo README
    
    * rename angular demo directory
    
    * remove 'TODO' comment from ngOnDestroy to match documenation
    
    * Update fluid-angular-demo/README.md
    
    Co-authored-by: Skyler Jokiel <skjokiel@microsoft.com>
    
    * rename folder to angular-demo
    
    Co-authored-by: Helio Liu <59622401+heliocliu@users.noreply.github.com>
    Co-authored-by: Skyler Jokiel <skjokiel@microsoft.com>

---
+++ angular-demo/src/main.ts
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
