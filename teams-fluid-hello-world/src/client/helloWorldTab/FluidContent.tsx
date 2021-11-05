32d9b8dffaa8f4d4b8589814ea9038843de8797a
Tim Wang <82841707+timtwang@users.noreply.github.com>
2021-11-05 17:21:54 -0500

    Added v1 for Fluid + Teams application (#345)
    * reverting to 013c8f64117ccce421cf6ce580a7f909814448c0
    
    * adding .env
    
    * removed typecasting and change Package.json name
    
    * removed env variable + added strongly typed diceView + variable name change
    
    * revert back tabName to entityId to reduce overhead in README
    
    * added cred comments + edited inTeams===false + updated build.yml
    
    * added licensing + test script
    
    * added test:report

---
+++ teams-fluid-hello-world/src/client/helloWorldTab/FluidContent.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Button } from "@fluentui/react-northstar";
+import { SharedMap } from "fluid-framework";
+import { diceValueKey } from "./Util";
+import { useState, useEffect } from "react";
+
+export type FluidContentProps = Readonly<{
+    fluidMap: SharedMap;
+}>
+
+export const FluidContent = (props : FluidContentProps) => {
+
+    const generateState = (): {color : string, content : string} => {
+        const diceValue = props.fluidMap.get(diceValueKey);
+        // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
+        return {
+            content: String.fromCodePoint(0x267f + diceValue),
+            color: `hsl(${diceValue * 60}, 70%, 30%)`
+        };
+    };
+    const [diceView, setDiceView] = useState<{color : string, content : string} | undefined >(generateState());
+
+    useEffect(() => {
+        // sync Fluid data into view state
+        const updateDice = () => {
+            setDiceView(generateState());
+        };
+        // Use the changed event to trigger the rerender whenever the value changes.
+        props.fluidMap.on("valueChanged", updateDice);
+
+        // turn off listener when component is unmounted
+        return () => { props.fluidMap.off("valueChanged", updateDice); };
+    }, [props.fluidMap]);
+
+    // Set the value at our diceValueKey with a random number between 1 and 6.
+    const onClick = () => {
+        props.fluidMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+    };
+
+    return (
+        <div>
+            <h2>Hello World!</h2>
+            <div className="wrapper" style={{ alignItems: "center" }} >
+                <div className="dice" style={{ color: diceView!.color, fontSize: "200px" }}>{diceView!.content}</div>
+                <Button className="roll" style={{ fontSize: "50px" }} onClick={onClick}> Roll </Button>
+            </div>
+        </div>
+    );
+};
---
+++ teams-fluid-hello-world/src/server/TeamsAppsComponents.ts
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the helloWorldTab tab
+export * from "./helloWorldTab/HelloWorldTab";
