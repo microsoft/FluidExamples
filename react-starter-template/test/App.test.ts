d0398d8e3cfb012fc8b78158192c9366593f28cd
sdeshpande3 <sdeshpande@microsoft.com>
2022-10-31 20:31:39 -0700

    test wip

---
+++ react-starter-template/test/App.test.ts
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const config = require("../jest.config");
+
+describe("react-starter-template", () => {
+    const load = async () => {
+        await page.goto(config.globals.URL, {
+            waitUntil: ["networkidle2", "load"],
+            timeout: 100000,
+        });
+    };
+
+    beforeEach(async () => {
+        await load();
+        expect(await page.title()).toBe("React App");
+        url = await page.url();
+    });
+
+    it("Load the container", async () => {
+        console.log("Container URL---", url);
+    });
+});
