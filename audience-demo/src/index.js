06835e04be64405e06b95410ecb7dcd92bfecd38
Sonali Deshpande <46719950+sonalivdeshpande@users.noreply.github.com>
2022-10-31 22:50:34 -0700

    Revert "Manage examples using lerna (#567)" (#578)

--- audience-demo/src/index.js
+++ audience-demo/src/index.js
@@ -4,12 +4,8 @@ Licensed under the MIT License.
 */
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root"),
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
