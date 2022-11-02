713eaef2227e6dc3a5209a08c032904af935ca9a
sdeshpande3 <sdeshpande@microsoft.com>
2022-11-02 15:14:13 -0700

    multi-framework-diceroller test infra

---
+++ multi-framework-diceroller/jest.config.js
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+    preset: "jest-puppeteer",
+    globals: {
+        URL: "http://localhost:8080",
+    },
+    verbose: true,
+    testTimeout: 20000,
+};
