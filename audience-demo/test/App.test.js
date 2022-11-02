1fc1c22b5a874803b4024bfa07c909da25ad8ed7
sdeshpande3 <sdeshpande@microsoft.com>
2022-11-02 14:27:21 -0700

    test infra in audience demo

---
+++ audience-demo/test/App.test.js
@@ -0,0 +1,27 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const config = require("../jest.config");
+
+let url;
+
+describe("audience-demo", () => {
+	const load = async () => {
+		await page.goto(config.globals.URL, {
+			waitUntil: ["networkidle2", "load"],
+			timeout: 100000,
+		});
+	};
+
+	beforeEach(async () => {
+		await load();
+		expect(await page.title()).toBe("Fluid Audience Example");
+		url = await page.url();
+	});
+
+	it("Load the container", async () => {
+		console.log("Container URL---", url);
+	});
+});
