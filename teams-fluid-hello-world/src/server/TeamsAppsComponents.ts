81c4c5db277dbc8cb1f9bcaac040611e9abe5ca9
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2023-05-01 14:26:25 -0700

    remove teams-fluid-hello-world example (#767)

--- teams-fluid-hello-world/src/server/TeamsAppsComponents.ts
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
- * Licensed under the MIT License.
- */
-
-// Components will be added here
-export const nonce = {}; // Do not remove!
-// Automatically added for the helloWorldTab tab
-export * from "./helloWorldTab/HelloWorldTab";
