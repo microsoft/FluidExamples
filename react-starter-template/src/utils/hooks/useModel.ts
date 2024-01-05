a7c6a5ec6bc71f75d13ee2d37c6022539722202e
Matt Rakow <marakow@microsoft.com>
2024-01-04 17:20:36 -0800

    Delete most demos

--- react-starter-template/src/utils/hooks/useModel.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import { ModelContext } from "../context/context";
-
-// Used by FluidContext, useDispatch and useSelector
-export const useModel = () => useContext(ModelContext);
