e6cae340d8f43694fcf61c2b54cc357c379c58ce
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-08-15 00:25:09 +0000

    test: No retries

--- item-counter/playwright.config.ts
+++ item-counter/playwright.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
 	// Fail the build on CI if you accidentally left test.only in the source code.
 	forbidOnly: !!process.env.CI,
 
-	// Retry on CI only.
-	retries: process.env.CI ? 2 : 0,
+	// Don't retry on failure.
+	retries: 0,
 
 	// Reporter to use
 	reporter: [
