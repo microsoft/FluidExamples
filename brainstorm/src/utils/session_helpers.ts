c8185bd10fc12e71bb0f1dfd7a6f8c747442c251
nmsimons <nick@dreamlarge.com>
2025-01-24 08:53:55 -0800

    clean up selection manager

--- brainstorm/src/utils/session_helpers.ts
+++ brainstorm/src/utils/session_helpers.ts
@@ -13,32 +13,69 @@ import type { Note } from "../schema/app_schema.js";
 import { selectAction } from "./utils.js";
 
 export class SelectionManager extends EventTarget {
-	private valueManager: BrainstormSelection;
+	statesName: `${string}:${string}` = "name:brainstorm-presence";
+
+	statesSchema = {
+		// sets selected to an array of strings
+		selected: Latest({ notes: [] as string[] }),
+	} satisfies PresenceStatesSchema;
+
+	private valueManager: PresenceStatesEntries<typeof this.statesSchema>["selected"];
 
 	constructor(presence: IPresence) {
 		super();
-		this.valueManager = presence.getStates(statesName, statesSchema).props.selected;
+		this.valueManager = presence.getStates(this.statesName, this.statesSchema).props.selected;
 		this.valueManager.events.on("updated", () =>
 			this.dispatchEvent(new Event("selectionChanged")),
 		);
 	}
 
 	public testNoteSelection(note: Note) {
-		return testNoteSelection(note, this.valueManager);
+		return this.valueManager.local.notes.indexOf(note.id) != -1;
 	}
 
 	public testNoteRemoteSelection(note: Note) {
-		return testNoteRemoteSelection(note, this.valueManager);
+		const remoteSelectedClients: string[] = [];
+
+		for (const cv of this.valueManager.clientValues()) {
+			if (cv.client.getConnectionStatus() === "Connected") {
+				if (cv.value.notes.indexOf(note.id) !== -1) {
+					remoteSelectedClients.push(cv.client.sessionId);
+				}
+			}
+		}
+
+		return remoteSelectedClients.length > 0;
 	}
 
 	public updateNoteSelection(note: Note, action: selectAction) {
 		// emit an event to notify the app that the selection has changed
 		this.dispatchEvent(new Event("selectionChanged"));
-		updateNoteSelection(note, action, this.valueManager);
+		if (action == selectAction.MULTI) {
+			const arr: string[] = this.valueManager.local.notes.slice();
+			const i = arr.indexOf(note.id);
+			if (i == -1) {
+				arr.push(note.id);
+			} else {
+				arr.splice(i, 1);
+			}
+			this.valueManager.local = { notes: arr };
+		}
+
+		if (action == selectAction.SINGLE) {
+			let arr: string[] = [];
+			const i = this.valueManager.local.notes.indexOf(note.id);
+			if (i == -1) {
+				arr = [note.id];
+			}
+			this.valueManager.local = { notes: arr };
+		}
+
+		return;
 	}
 
 	public getSelectedNotes() {
-		return getSelectedNotes(this.valueManager);
+		return this.valueManager.local.notes;
 	}
 
 	public dispose() {
@@ -47,62 +84,3 @@ export class SelectionManager extends EventTarget {
 		);
 	}
 }
-
-const statesName = "name:brainstorm-presence";
-
-const statesSchema = {
-	// sets selected to an array of strings
-	selected: Latest({ notes: [] as string[] }),
-} satisfies PresenceStatesSchema;
-
-type BrainstormSelection = PresenceStatesEntries<typeof statesSchema>["selected"];
-
-const testNoteSelection = (note: Note, latestValueManager: BrainstormSelection): boolean => {
-	return latestValueManager.local.notes.indexOf(note.id) != -1;
-};
-
-const testNoteRemoteSelection = (note: Note, latestValueManager: BrainstormSelection): boolean => {
-	const remoteSelectedClients: string[] = [];
-
-	for (const cv of latestValueManager.clientValues()) {
-		if (cv.client.getConnectionStatus() === "Connected") {
-			if (cv.value.notes.indexOf(note.id) !== -1) {
-				remoteSelectedClients.push(cv.client.sessionId);
-			}
-		}
-	}
-
-	return remoteSelectedClients.length > 0;
-};
-
-const updateNoteSelection = (
-	note: Note,
-	action: selectAction,
-	latestValueManager: BrainstormSelection,
-) => {
-	if (action == selectAction.MULTI) {
-		const arr: string[] = latestValueManager.local.notes.slice();
-		const i = arr.indexOf(note.id);
-		if (i == -1) {
-			arr.push(note.id);
-		} else {
-			arr.splice(i, 1);
-		}
-		latestValueManager.local = { notes: arr };
-	}
-
-	if (action == selectAction.SINGLE) {
-		let arr: string[] = [];
-		const i = latestValueManager.local.notes.indexOf(note.id);
-		if (i == -1) {
-			arr = [note.id];
-		}
-		latestValueManager.local = { notes: arr };
-	}
-
-	return;
-};
-
-const getSelectedNotes = (latestValueManager: BrainstormSelection): readonly string[] => {
-	return latestValueManager.local.notes;
-};
